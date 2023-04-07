0efe9415dc69b03c97bc2133c136acaf4394a68b
HVKammen <huubvkammen@gmail.com>
2023-04-07 21:18:16 +0200

    info landen klaar gezet

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-export default function Article() {
-    return <div>Article</div>;
+export default function Article({ flags, name, population, region, subregion }) {
+    return (
+    <>
+       <article>
+          <img src={flags.svg} alt="" />
+          <h2>{name.common}</h2>
+          <ul>
+            <li>Population: {population}</li>
+            <li>Region: {region}</li>
+            <li>Subregion: {subregion}</li>
+          </ul>
+       </article>
+    </>
+  );
 }
