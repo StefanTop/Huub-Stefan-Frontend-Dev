5754fcbc1e0f8986f83f05738c6dd4ed481bc78c
HVKammen <huubvkammen@gmail.com>
2023-04-07 20:54:04 +0200

    Opmaak loading en Article comp

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,4 +1,36 @@
+import {useState, useEffect} from "react"
+
 export default function Countries()
 {     
-    return <>Countries</>; 
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        const getCountries = async() => {
+            try {
+                const res = await fetch("https://restcountries.com/v3.1/all")
+                const data = await res.json()
+                setCountries(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getCountries
+    }, [])
+
+    return (
+       <>
+         {!countries ? (
+           <h1 className="text-gray-900 font-bold uppercase tracking-wide flex items-center justify-center text-center h-screen text-4xl dark:text-white">
+              Loading...
+           </h1> 
+         ) : (
+        <section>
+            {countries.map((country) => (
+              <Article key={country.name.common} {...country} />
+            ))}
+        </section>
+      )} 
+    </>
+  );
 }
