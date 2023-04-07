8ae803923e2eb61287f5e29c2330035b12eed1b3
HVKammen <huubvkammen@gmail.com>
2023-04-08 00:35:20 +0200

    Error pagina removed, is niet  nodig

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Countries from "./components/Countries";
 import SingleCountry from "./components/SingleCountry";
-import Error from "./components/Error";
 
 function App() {
   return (
@@ -9,10 +8,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Countries />}></Route>
         <Route path="/:name" element={<SingleCountry />}></Route>
-        <Route path="*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
