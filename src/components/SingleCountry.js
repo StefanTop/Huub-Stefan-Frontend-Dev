9e684c3c5f2e5909e7cfd2282fa601c4d2634a84
HVKammen <huubvkammen@gmail.com>
2023-04-08 00:18:36 +0200

    Styling SingleCountry

--- src/components/SingleCountry.js
+++ src/components/SingleCountry.js
@@ -30,11 +30,11 @@ export default function SingleCountry() {
                     </article>
 
                     <article>
-                        <h1 className="font-bold text-gray-900 text-4xl lg:text-6xl">
+                        <h1 className="mb-8 font-bold text-gray-900 dark:text-white text-4xl lg:text-6xl">
                             {item.name.official}
                         </h1>
                         
-                        <ul>
+                        <ul className="my-4 flex flex-col items-start justify-start gap-2 text-slate-700 dark:text-gray-400">
                             <li>Capital: {item.capital[0]}</li>
                             <li>Population: {item.population.toLocaleString()}</li>
                             <li>Region: {item.region}</li>
@@ -43,10 +43,10 @@ export default function SingleCountry() {
 
                         {item.borders && (
                         <>
-                           <h3>Borders:</h3>
-                           <ul>
+                           <h3 className="text-gray-900 font-bold text-lg mb-2 dark:text-white">Borders:</h3>
+                           <ul className="flex flex-wrap items-start justify-start gap-2">
                              {item.borders.map((border, index) => (
-                                <li key={index}>{border}</li>
+                                <li key={index} className="bg-white p-2 rounded text-xs tracking-wide shadow dark:bg-gray-800 dark:text-gray-400 text-gray-700">{border}</li>
                             ))}
                         </ul>
                         </>
