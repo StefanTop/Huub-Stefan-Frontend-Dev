8bc65a24d96cff8cb44f886b47ce40c95f665bcb
StefanTop <Stefantop@me.com>
2023-04-07 23:12:18 +0200

    Doorlink mogelijkheid naar landen eerste info erop

--- src/components/SingleCountry.js
+++ src/components/SingleCountry.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function SingleCountry() {
-  return (
-    <div>SingleCountry</div>
-  )
+    const[country, setCountry] = useState([]);
+    const {name} = useParams();
+
+    useEffect(() => {
+        const getSingleCountry = async() => {
+            try {
+                const res = await fetch(`https://restcountries.com/v3.1/name/${name}`)
+                const data = await res.json()
+                setCountry(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getSingleCountry()
+    }, [name]);
+
+  return <>
+    <section>
+            {country.map((item) => (
+                <div key={item.population}>
+                    <article>
+                        <img src={item.flags.svg} alt={item.name.common} />
+                    </article>
+
+                    <article>
+                        <h1>{item.name.official}</h1>
+                    </article>
+                </div>
+            ))}
+    </section>
+  </>;
 }
