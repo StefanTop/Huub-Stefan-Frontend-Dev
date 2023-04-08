371c9cde2707150391d74dc81f7763b238a3f21d
StefanTop <Stefantop@me.com>
2023-04-08 14:55:25 +0200

    Search functie en filter by region functie toegevoegd

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,7 +3,27 @@ import Article from "./Article";
 
 export default function Countries() {     
     const [countries, setCountries] = useState([]);
-    
+    const [searchText, setSearchText] = useState("");
+    const regions = [
+        {
+            name: "Europe",
+        },
+        {
+            name: "Asia",
+        },
+        {
+            name: "Africa",
+        },
+        {
+            name: "Oceania",
+        },
+        {
+            name: "Americas",
+        },
+        {
+            name: "Antartic",
+        },
+    ]
 
     useEffect(() => {
         const getCountries = async() => {
@@ -17,7 +37,37 @@ export default function Countries() {
         };
 
         getCountries();
-    }, [])
+    }, []);
+
+    async function searchCountry() {
+        try {
+            const res = await fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+            const data = await res.json()
+            setCountries(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async function filterByRegion(region) {
+        try {
+            const res = await fetch(`https://restcountries.com/v3.1/region/${region}`)
+            const data = await res.json()
+            setCountries(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    function handleSearchCountry(e) {
+        e.preventDefault();
+        searchCountry();
+    }
+
+    function handleFilterByRegion(e){
+        e.preventDefault();
+        handleFilterByRegion()
+    }
 
     return (
        <>
@@ -29,24 +79,30 @@ export default function Countries() {
         <section className="container mx-auto p-8">
             {/* form */}
             <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
-                <form autoComplete="off"className="max-w-4xl  md:flex-1"> 
+                <form onSubmit={handleSearchCountry} autoComplete="off"className="max-w-4xl  md:flex-1"> 
                     <input 
                     type="text" 
                     name="search" 
                     id="search" 
                     placeholder="Search for a country by its name" 
                     required
-                    className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow rounded outline-none" 
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                    className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow rounded outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200" 
                     />
                 </form>
 
-                <form>
+                <form onSubmit={handleFilterByRegion}>
                     <select 
                     name="filter-by-region" 
                     id="filter-by-region" 
-                    className="w-52 py-3 px-4 outline-none shadow rounded"
+                    className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700"
+                    value={regions.name}
+                    onChange={e => filterByRegion(e.target.value)}
                     >
-                        {/*  */}
+                        {regions.map((region, index) => (
+                            <option key={index} value={region.name}>{region.name}</option>
+                        ))}
                     </select>
                 </form>
             </div>
